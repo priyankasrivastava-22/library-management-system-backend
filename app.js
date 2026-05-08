c743b3b0618e9ce9a5324d488d60d9e7c68a55db
Priyanka Srivastava <priyankarenu20@gmail.com>
2026-05-08 20:52:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,11 +13,8 @@ const PORT = process.env.PORT || 5000;
 const db = require("./config/db");
 
 // ================= MIDDLEWARE =================
-app.use(cors({
-  origin: '*', // This allows any website to talk to backend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
+
 app.use(express.json());   
 
 // ================= ROUTE IMPORTS =================
