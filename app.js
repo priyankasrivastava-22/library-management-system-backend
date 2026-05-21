1c73d13fba62cd34f9fd549218d08f844aa389a4
priyankasrivastava-22 <priyankarenu20@gmail.com>
2026-05-21 08:26:37 +0000

    Fix CORS for test frontend

--- app.js
+++ app.js
@@ -13,7 +13,20 @@ const PORT = process.env.PORT || 5000;
 const db = require("./config/db");
 
 // ================= MIDDLEWARE =================
-app.use(cors());
+const allowedOrigins = [
+  "https://library-management-system-lms-frontend.onrender.com",
+  "https://lms-frontend-testing.onrender.com",
+  "https://lms-frontend-dev.onrender.com",
+  "http://localhost:3000",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());   
 
